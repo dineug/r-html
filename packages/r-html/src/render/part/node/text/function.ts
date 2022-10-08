710002609de0cb46f78b081785a633fe27320f54
seunghwan-lee94 <seunghwan.lee94@nhndooray.com>
2022-10-08 21:17:10 +0900

    fix: functionPart directive

--- packages/r-html/src/render/part/node/text/function.ts
+++ packages/r-html/src/render/part/node/text/function.ts
@@ -1,73 +1,7 @@
-import { DIRECTIVE } from '@/constants';
-import { isArray, isFunction, isNull, isUndefined } from '@/helpers/is-type';
-import { DirectiveType } from '@/render/directives';
-import {
-  NodeDirective,
-  NodeDirectiveClass,
-} from '@/render/directives/nodeDirective';
-import { rangeNodes, removeNode } from '@/render/helper';
 import { Part } from '@/render/part';
-import {
-  createPart,
-  getPartType,
-  isPart,
-} from '@/render/part/node/text/helper';
-
-const isDirective = (value: any) =>
-  Reflect.get(value, DIRECTIVE) === DirectiveType.node;
 
 export class FunctionPart implements Part {
-  #startNode: Comment;
-  #endNode: Comment;
-  #DirectiveClass: NodeDirectiveClass | null = null;
-  #directive: NodeDirective | null = null;
-  #part: Part | null = null;
+  constructor(startNode: Comment, endNode: Comment) {}
 
-  constructor(startNode: Comment, endNode: Comment) {
-    this.#startNode = startNode;
-    this.#endNode = endNode;
-  }
-
-  commit(value: any) {
-    if (!isFunction(value)) return;
-
-    const directiveTuple = value();
-    if (!isArray(directiveTuple)) return;
-
-    const [DirectiveClass, args] = directiveTuple;
-    if (!isFunction(DirectiveClass) || !isArray(args)) return;
-
-    if (this.#DirectiveClass !== DirectiveClass) {
-      this.clear();
-      const directive = new DirectiveClass({
-        startNode: this.#startNode,
-        endNode: this.#endNode,
-      });
-
-      if (!isDirective(directive)) return;
-      this.#directive = directive;
-      this.#DirectiveClass = DirectiveClass;
-    }
-
-    const result = this.#directive?.render(args);
-    if (isUndefined(result)) return;
-
-    const type = getPartType(result);
-    if (!isPart(type, this.#part)) {
-      isNull(this.#part) || this.clear();
-      this.#part = createPart(type, this.#startNode, this.#endNode);
-    }
-
-    this.#part?.commit(result);
-  }
-
-  clear() {
-    this.#directive?.destroy();
-    this.#part?.destroy?.();
-    rangeNodes(this.#startNode, this.#endNode).forEach(removeNode);
-  }
-
-  destroy() {
-    this.clear();
-  }
+  commit(value: any) {}
 }
