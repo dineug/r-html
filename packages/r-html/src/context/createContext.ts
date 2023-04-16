b5e6b2852d1917bb28193baa9140c988997af962
dineug <dineug2@gmail.com>
2023-04-16 18:53:29 +0900

    fix createContext default key

--- packages/r-html/src/context/createContext.ts
+++ packages/r-html/src/context/createContext.ts
@@ -3,8 +3,8 @@ export type Context<T> = {
   value: T;
 };
 
-export function createContext<T>(key: string | symbol, value: T): Context<T> {
-  return Object.freeze({ key, value });
+export function createContext<T>(value: T, key?: string | symbol): Context<T> {
+  return Object.freeze({ key: key ?? Symbol(), value });
 }
 
 export const contextSubscribe = '@@r-html/context-subscribe' as const;
